d5785facca35e40f836b2af9f7c1aaa1c059b6ba
Dmitry Rum <acopalypse@yandex.ru>
2021-10-07 15:45:55 +0300

    add apollo config, graphql request

---
+++ src/other/getChatactersByName.tsx
@@ -0,0 +1,13 @@
+import { gql } from '@apollo/client';
+
+export const GET_CHARACTERS = gql`
+  query getCharacterByName($name: String!) {
+    characters(filter: { name: $name }) {
+      results {
+        id
+        name
+        image
+      }
+    }
+  }
+`;
