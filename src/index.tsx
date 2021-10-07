16594712a37120e7f01a2eef156e7446d9fe1e0b
Dmitry Rum <acopalypse@yandex.ru>
2021-10-07 15:57:52 +0300

    add basic logic variables

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { App } from './App';
 import reportWebVitals from './reportWebVitals';
+import { ApolloProvider } from '@apollo/client';
+import { client } from './other/apollo.client';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root'),
 );
 reportWebVitals();
